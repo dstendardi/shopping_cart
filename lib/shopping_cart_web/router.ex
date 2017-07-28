132c8d5f67829dd7328ec20bcec027324b8f8623
David Stendardi <david.stendardi@instaply.com>
2017-07-28 08:59:25 +0200

    Generate new phoenix project.
    Used the command line `mix phx.new shopping_cart --no-html --no-brunch --binary-id`
    
    * --no-html && --no-brunch : if I go for ux development, then I will relay on webpack or elm
    * --binary-id : I prefer to not relay on side effect incremental ids.

---
+++ lib/shopping_cart_web/router.ex
@@ -0,0 +1,11 @@
+defmodule ShoppingCartWeb.Router do
+  use ShoppingCartWeb, :router
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", ShoppingCartWeb do
+    pipe_through :api
+  end
+end
