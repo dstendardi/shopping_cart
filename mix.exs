8537abda1f495b0e3a993977904a2bbc4236d319
David Stendardi <david.stendardi@instaply.com>
2017-08-02 15:06:22 +0200

    downgrade elixir

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Acme.Mixfile do
     [
       app: :acme,
       version: "0.0.1",
-      elixir: "~> 1.5",
+      elixir: "~> 1.4.5",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
